f7512a34ebd20489953c9d651980c934abd78602
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2024-01-09 11:20:10 +0100

    Interface erstellt

--- Logbook.Lib/Entry.cs
+++ Logbook.Lib/Entry.cs
@@ -1,5 +1,8 @@
 ﻿namespace Logbook.Lib
 {
+    /// <summary>
+    /// Create an Entry for the logbook
+    /// </summary>
     public class Entry
     {
         public string Description { get; set; } = string.Empty;
@@ -18,12 +21,26 @@
         
         public string To { get; set; }
 
+        /// <summary>
+        /// create a number as an ID
+        /// </summary>
         public string ID { get; set; }
 
 
-        public Entry(string ID, DateTime start, DateTime end, int startkm, int endkm, string numberplate, string from, string to)
+        public Entry(DateTime start, DateTime end, int startkm, int endkm, string numberplate, string from, string to, string ID)
         {
             this.ID = ID;
+            this.Start = start;
+            this.End = end;
+            this.StartKM = startkm;
+            this.EndKM = endkm;
+            this.NumberPlate = numberplate;
+            this.From = from;
+            this.To = to;
+        }
+        public Entry(DateTime start, DateTime end, int startkm, int endkm, string numberplate, string from, string to)
+        {
+            this.ID = new Guid().ToString();
             this.Start = start;
             this.End = end;
             this.StartKM = startkm;
---
+++ Logbook.Lib/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logbook.Lib
+{
+    public interface IRepository
+    {
+        bool add(Entry entry);
+        bool delete(Entry entry);
+        bool update(Entry entry);
+        bool save();
+
+        List<Entry> GetAll();
+    }
+}
