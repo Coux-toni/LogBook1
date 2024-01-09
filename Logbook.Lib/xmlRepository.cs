912088f3993ee225cca1f4aaa216fd466d85c4ef
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2024-01-09 11:22:44 +0100

    xml und csv repository wurden erstellt

---
+++ Logbook.Lib/xmlRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logbook.Lib
+{
+    public class xmlRepository : IRepository
+    {
+        public bool add(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entry> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool update(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
