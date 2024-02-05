fe50c147578057dfa330e002c9f474eb04c13da9
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2024-02-05 12:17:00 +0100

    Code vereinfacht

--- LogBook.Cli/Program.cs
+++ LogBook.Cli/Program.cs
@@ -5,11 +5,11 @@ Console.WriteLine("Willkommen beim Fahrtenbuch");
 IRepository repository = new memoryRepository();
 List<Entry> entries = repository.GetAll();
 
-repository.add(new Entry(DateTime.Now, DateTime.Now.AddHours(2).AddMinutes(22), 25000, 25178, "ZE-XYZ123", "Zell am See", "München"));
+repository.Add(new Entry(DateTime.Now, DateTime.Now.AddHours(2).AddMinutes(22), 25000, 25178, "ZE-XYZ123", "Zell am See", "München"));
 Entry entrySaalfelden = new Entry(DateTime.Now.AddDays(3), DateTime.Now.AddDays(3).AddMinutes(20),
                                   25500, 25514, "ZE-XYZ123", "Zell am See", "Saalfelden");
 
-repository.add(entrySaalfelden);
+repository.Add(entrySaalfelden);
 
  foreach (Entry entry in entries)
  {
