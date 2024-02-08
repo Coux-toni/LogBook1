dddc9cee18e57d96513bd8cd753e66257f9f2c45
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2024-02-08 08:00:07 +0100

    add Descripiton to Entry

--- LogBook.Cli/Program.cs
+++ LogBook.Cli/Program.cs
@@ -8,7 +8,10 @@ List<Entry> entries = repository.GetAll();
 
 repository.Add(new Entry(DateTime.Now, DateTime.Now.AddHours(2).AddMinutes(22), 25000, 25178, "ZE-XYZ123", "Zell am See", "München"));
 Entry entrySaalfelden = new Entry(DateTime.Now.AddDays(3), DateTime.Now.AddDays(3).AddMinutes(20),
-                                  25500, 25514, "ZE-XYZ123", "Zell am See", "Saalfelden");
+                                  25500, 25514, "ZE-XYZ123", "Zell am See", "Saalfelden")
+{ 
+    Description = "Fahrt nach Saalfelden"
+};
 
 repository.Add(entrySaalfelden);
 
