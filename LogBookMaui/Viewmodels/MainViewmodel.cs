10c0c1806a5fb6dd214440404fdf143411778c84
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2024-02-19 12:16:21 +0100

    add entries collection and load command and add maui toolkit

--- LogBookMaui/Viewmodels/MainViewmodel.cs
+++ LogBookMaui/Viewmodels/MainViewmodel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using Logbook.Lib;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,12 @@ namespace LogBookMaui.Viewmodels
         public string Header => "Fahrtenbuch";
         IRepository _repository = repository;
 
+        ObservableCollection<Logbook.Lib.Entry> _entries = [];
 
+        [RelayCommand]
+        void LoadData()
+        {
+
+        }
     }
 }
